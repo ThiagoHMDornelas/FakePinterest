c81bd8f3c7b75c7ca974bd192514a6397dab67c2
ThiagoHMDornelas <thiagohmdornelas@gmail.com>
2025-05-27 16:28:25 -0300

    novas atualizações

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,5 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+#import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'e09cd9eddebc52208c670ea53536bf90'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'homepage' #passar o nome do route q gerencia o login. neste caso, o login q feito no "def homepage()"
+# login.login_message = 'faça o login para continuar'
+# login.login_message_category = 'alert-info'
 
 from fakepinterest import routes
--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,11 +1,21 @@
 # criar as rotas para o site (os links)
 from flask import render_template, url_for
 from fakepinterest import app
+from flask_login import login_required
+from fakepinterest.forms import FormLogin, FormCriarConta
 
-@app.route("/")
+@app.route("/",methods=["GET", "POST"])
 def homepage():
-    return render_template("homepage.html")
+    form_login = FormLogin()
+    return render_template("homepage.html", form=form_login)
+
+
+@app.route("/criarconta", methods=["GET", "POST"])
+def criarconta():
+    form_criar_conta = FormCriarConta()
+    return render_template("criarconta.html", form=form_criar_conta)
 
 @app.route("/perfil/<usuario>")
+@login_required
 def perfil(usuario):
     return render_template("perfil.html", usuario=usuario)
