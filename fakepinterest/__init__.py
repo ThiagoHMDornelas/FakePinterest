37bf08753d7be8cf38bae92e6bda99f2a7765776
ThiagoHMDornelas <thiagohmdornelas@gmail.com>
2025-05-30 14:09:34 -0300

    finalização do projeto

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -7,11 +7,12 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e09cd9eddebc52208c670ea53536bf90'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'homepage' #passar o nome do route q gerencia o login. neste caso, o login q feito no "def homepage()"
+login_manager.login_view = 'homepage' # passar o nome do route q gerencia o login. neste caso, o login q feito no "def homepage()"
 # login.login_message = 'faça o login para continuar'
 # login.login_message_category = 'alert-info'
 
