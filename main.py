f9c04d761db228efd53b7a033f2a7bfeabbb7f1c
ThiagoHMDornelas <thiagohmdornelas@gmail.com>
2025-06-02 09:57:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os
 from fakepinterest import app
 
+# Verificando se está rodando ONLINE com uma variável de ambiente
+IS_ONLINE = os.getenv("IS_ONLINE", "false").lower() == "true"
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=IS_ONLINE)
